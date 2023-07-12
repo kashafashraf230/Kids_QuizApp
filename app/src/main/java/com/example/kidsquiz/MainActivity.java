4b092b1f7f65397dc84ff59dfa6ade4f450c512e
kashaf_ashraf230 <kashafashraf412@gmail.com>
2023-07-12 22:25:34 +0500

    Fragments Done

--- app/src/main/java/com/example/kidsquiz/MainActivity.java
+++ app/src/main/java/com/example/kidsquiz/MainActivity.java
@@ -1,14 +1,74 @@
 package com.example.kidsquiz;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Fragment {
+
+    Button btnOne, btnTwo, btnThree;
+   // DBHelper db;
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    ActionBarDrawerToggle toggle;
+
+    //List<Question> list = new ArrayList<Question>();
+
+    public MainActivity(){}
+
+    @SuppressLint("MissingInflatedId")
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.activity_main, container, false);
+        btnOne = view.findViewById(R.id.startButton);
+        //btnTwo = view.findViewById(R.id.resultbtn);
+       // db = new DBHelper(getContext().getApplicationContext());
+
+
+        /*btnOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                db.clearDB();
+                FRExam fragmentOne = new FRExam();
+                FragmentTransaction transaction =getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.layoutMainActivity, fragmentOne);
+                transaction.commit();
+            }
+        });*/
+
+        btnTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ResultDisplay fragmentOne = new ResultDisplay();
+                FragmentTransaction transaction =getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.layoutMainActivity, fragmentOne);
+                transaction.commit();
+
+
+            }
+        });
+
+
+        return view;
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/kidsquiz/ResultDisplay.java
@@ -0,0 +1,11 @@
+package com.example.kidsquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+public class ResultDisplay extends Fragment {
+
+
+}
